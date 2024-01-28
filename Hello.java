229abf240e012f820cb39b7c5b4fc343c06eabe1
Koustav Das <kdkdas8@gmail.com>
2024-01-28 15:31:14 +0530

    Initial Commit

---
+++ Hello.java
@@ -0,0 +1,24 @@
+class Hello {
+    public static void main (String args[]){
+
+        int num1 = 10;
+        int num2 = 15;
+        int result = num1 + num2;
+        long lo = 50l;
+        float fl = 5.8f;
+        double dub = 12e10;
+        char name = 'C'; // Takes only single  quote
+        boolean k = true;
+        System.out.println(result);
+        System.out.println(name);
+        System.out.println(dub);
+
+        byte b = 127;
+
+        int a = 277;
+
+        b  = (byte)a;
+
+        System.out.println(b);
+    }
+}
