8d21d0b84aa1b81626d640ab240dfbe49a100a5b
Koustav Das <kdkdas8@gmail.com>
2024-02-04 01:22:00 +0530

    updates

---
+++ Array.java
@@ -0,0 +1,16 @@
+public class Array {
+
+    public static void main(String args[])
+    {
+
+        int num[] = {3,7,2,4};
+
+        num[3] = 5;
+
+        for (int i = 0; i<4;i++){
+            System.out.println(num[i]);
+        }
+
+
+    }
+}
